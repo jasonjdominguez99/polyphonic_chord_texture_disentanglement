cda09a58e29d5966b66eae4912f605da03ea5384
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2022-06-08 17:34:40 +0100

    Added model hyperparameters to config file

--- train.py
+++ train.py
@@ -16,75 +16,121 @@ from amc_dl.torch_plus import (
 from amc_dl.torch_plus.train_utils import kl_anealing
 import torch
 from torch import optim
+import json
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-readme_fn = './train.py'
-batch_size = 128
-n_epoch = 6
-clip = 1
-parallel = False
-weights = [1, 0.5]
-beta = 0.1
-tf_rates = [(0.6, 0), (0.5, 0), (0.5, 0)]
-lr = 1e-3
-name = 'disvae-nozoth'
+def get_hyperparameters(config_file_path):
+    with open(config_file_path) as f:
+        args = json.load(f)
+    
+    return args
 
-parallel = (
-    parallel
-    if torch.cuda.is_available() and torch.cuda.device_count() > 1
-    else False
-)
-# train_model
-chd_encoder = RnnEncoder(36, 1024, 256)
-rhy_encoder = PtvaeEncoder(
-    device=device, z_size=256,
-    max_pitch=39 - 8, min_pitch=0
-)
-# rhy_encoder = TextureEncoder(256, 1024, 256)
-# pt_encoder = PtvaeEncoder(device=device, z_size=152)
-chd_decoder = RnnDecoder(z_dim=256)
-pt_decoder = PtvaeDecoder(note_embedding=None,
-                          dec_dur_hid_size=64, z_size=512)
-model = DisentangleVAE(name, device, chd_encoder,
-                       rhy_encoder, pt_decoder, chd_decoder)
 
-# data loaders
-data_loaders = (
-    MusicDataLoaders.get_loaders(
-        SEED, bs_train=batch_size, bs_val=batch_size,
-        portion=8, shift_low=-6, shift_high=5,
-        num_bar=2,contain_chord=True
+def main():
+    model_config_path = "poly_chord_model_config.json"
+    args = get_hyperparameters(model_config_path)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    parallel = args["parallel"] == "True"       # converting from json string to bool
+    parallel = (
+        parallel
+        if torch.cuda.is_available() and torch.cuda.device_count() > 1
+        else False
     )
-)
 
-log_path_mng = LogPathManager(readme_fn)
+    # difine model
+    chd_encoder = RnnEncoder(
+        args["chd_encoder_input_dim"],
+        args["chd_encoder_hidden_dim"],
+        args["chd_encoder_z"]
+    )
+    rhy_encoder = PtvaeEncoder(
+        device=device, z_size=args["rhy_encoder_z"],
+        max_pitch=args["rhy_encoder_max_pitch"] - 8,
+        min_pitch=args["rhy_encoder_min_pitch"]
+    )
 
-optimizer = optim.Adam(model.parameters(), lr=lr)
-scheduler = MinExponentialLR(optimizer, gamma=0.9999, minimum=1e-5)
-optimizer_scheduler = OptimizerScheduler(optimizer, scheduler, clip)
+    # rhy_encoder = TextureEncoder(256, 1024, 256)
+    # pt_encoder = PtvaeEncoder(device=device, z_size=152)
 
-writer_names = [
-    'loss', 'recon_loss', 'pl', 'dl', 'kl_loss', 'kl_chd',
-    'kl_rhy', 'chord_loss', 'root_loss', 'chroma_loss', 'bass_loss'
-]
-#, 'chord', 'root', 'chroma', 'bass']
-tags = {'loss': None}
-summary_writers = SummaryWriters(writer_names, tags, log_path_mng.writer_path)
-tfr1_scheduler = TeacherForcingScheduler(*tf_rates[0])
-tfr2_scheduler = TeacherForcingScheduler(*tf_rates[1])
-tfr3_scheduler = TeacherForcingScheduler(*tf_rates[2])
-weights_scheduler = ConstantScheduler(weights)
-beta_scheduler = TeacherForcingScheduler(beta, 0., f=kl_anealing)
-params_dic = dict(tfr1=tfr1_scheduler, tfr2=tfr2_scheduler,
-                  tfr3=tfr3_scheduler,
-                  beta=beta_scheduler, weights=weights_scheduler)
-param_scheduler = ParameterScheduler(**params_dic)
+    chd_decoder = RnnDecoder(z_dim=args["chd_decoder_z"])
+    pt_decoder = PtvaeDecoder(
+        note_embedding=None,
+        dec_dur_hid_size=args["pt_decoder_hid_size"],
+        z_size=args["pt_decoder_z"]
+    )
 
-training = TrainingVAE(device, model, parallel, log_path_mng,
-                       data_loaders, summary_writers, optimizer_scheduler,
-                       param_scheduler, n_epoch)
-training.run()
+    model = DisentangleVAE(
+        args["name"], device, 
+        chd_encoder, rhy_encoder, 
+        pt_decoder, chd_decoder
+    )
+
+    # data loaders
+    data_loaders = (
+        MusicDataLoaders.get_loaders(
+            SEED,
+            bs_train=args["batch_size"],
+            bs_val=args["batch_size"],
+            portion=args["loader_portion"],
+            shift_low=args["loader_shift_low"],
+            shift_high=args["loader_shift_high"],
+            num_bar=args["loader_num_bar"],
+            contain_chord=True
+        )
+    )
+
+    log_path_mng = LogPathManager(args["readme_fn"])
+
+    optimizer = optim.Adam(model.parameters(), lr=args["lr"])
+    scheduler = MinExponentialLR(
+        optimizer, gamma=args["sched_gamma"], minimum=args["sched_min"]
+    )
+    optimizer_scheduler = OptimizerScheduler(
+        optimizer, scheduler, args["clip"]
+    )
+
+    writer_names = [
+        'loss', 'recon_loss', 'pl', 'dl', 'kl_loss', 'kl_chd',
+        'kl_rhy', 'chord_loss', 'root_loss', 'chroma_loss', 'bass_loss'
+    ]
+    #, 'chord', 'root', 'chroma', 'bass']
+    tags = {'loss': None}
+    summary_writers = SummaryWriters(
+        writer_names, tags, log_path_mng.writer_path
+    )
+
+    tf_rates = [
+        (args["tf_rate1_1"], args["tf_rate1_2"]),
+        (args["tf_rate2_1"], args["tf_rate2_2"]),
+        (args["tf_rate3_1"], args["tf_rate3_2"])
+    ]
+    tfr1_scheduler = TeacherForcingScheduler(*tf_rates[0])
+    tfr2_scheduler = TeacherForcingScheduler(*tf_rates[1])
+    tfr3_scheduler = TeacherForcingScheduler(*tf_rates[2])
+
+    weights_scheduler = ConstantScheduler(
+        [args["weight1"], args["weight2"]]
+    )
+    beta_scheduler = TeacherForcingScheduler(
+        args["beta"], 0., f=kl_anealing
+    )
+
+    params_dic = dict(
+        tfr1=tfr1_scheduler, tfr2=tfr2_scheduler,
+        tfr3=tfr3_scheduler, beta=beta_scheduler,
+        weights=weights_scheduler
+    )
+    param_scheduler = ParameterScheduler(**params_dic)
+
+    # train model
+    training = TrainingVAE(
+        device, model, parallel, log_path_mng,
+        data_loaders, summary_writers, optimizer_scheduler,
+        param_scheduler, args["n_epoch"]
+    )
+    training.run()
 
 if __name__ == '__main__':
-    pass
+    main()
